bcc63b88a2ee8172b7acc1caf1ad274c2c57b097
jomarbarnobal <jomar.barnobal@outlook.com>
2018-05-26 16:42:56 +0800

    add medium editor js and medium editor css

--- spec/rails_medium_editor_insert_plugin_spec.rb
+++ spec/rails_medium_editor_insert_plugin_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe RailsMediumEditorInsertPlugin do
     expect(RailsMediumEditorInsertPlugin::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
---
+++ vendor/assets/javascripts/rails-medium-editor-insert-plugin.js
@@ -0,0 +1,7 @@
+//=require medium_editor/handlebars
+//=require medium_editor/jquery-fileupload
+//=require medium_editor/jquery-iframe-transport
+//=require medium_editor/jquery-sortable
+//=require medium_editor/jquery-ui-widget
+//=require medium_editor/medium-editor-insert-plugin
+//=require medium_editor/medium-editor
