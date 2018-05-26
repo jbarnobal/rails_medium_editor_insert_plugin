9ece76033c81c7e95cf6268a0642fb4ec9b7e0e8
jomarbarnobal <jomar.barnobal@outlook.com>
2018-05-26 19:08:41 +0800

    remove unuse file

--- vendor/assets/javascripts/rails-medium-editor-insert-plugin.js
+++ vendor/assets/javascripts/rails-medium-editor-insert-plugin.js
@@ -1,7 +1,7 @@
-//=require medium_editor_js/handlebars
-//=require medium_editor_js/jquery-fileupload
-//=require medium_editor_js/jquery-iframe-transport
-//=require medium_editor_js/jquery-sortable
-//=require medium_editor_js/jquery-ui-widget
-//=require medium_editor_js/medium-editor-insert-plugin
-//=require medium_editor_js/medium-editor
+//= require medium_editor_js/handlebars
+//= require medium_editor_js/medium-editor
+//= require medium_editor_js/medium-editor-insert-plugin
+//= require medium_editor_js/jquery-ui-widget
+//= require medium_editor_js/jquery-sortable
+//= require medium_editor_js/jquery-fileupload
+//= require medium_editor_js/jquery-iframe-transport
